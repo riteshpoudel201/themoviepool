e39ef9aa8fa37be7c85e55ffb32ea800144d8e02
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-09 15:05:47 +0545

    add: created the similar card component to show similar shows

---
+++ src/components/cards/SimilarCard.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { formatYear } from "../../utils/dateUtils";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+/* eslint-disable react/prop-types */
+const SimilarCard = ({
+  imgAlt,
+  show,
+  showType = "movie",
+}) => {
+  return (
+    <Link to={`/show/${showType}/${show?.id}`}>
+      <div className="relative w-full h-[100px] lg:h-[80px] rounded-lg shadow-lg overflow-hidden">
+        {/* Movie Poster */}
+        <img
+          src={`${base_url}${show?.backdrop_path}`}
+          alt={imgAlt}
+          className="w-full h-full object-cover transition-transform duration-300 group-hover/card:scale-110 absolute"
+        />
+
+        {/* Overlay */}
+        <div className="w-full h-full py-2 inset-0 bg-black/60 flex flex-col justify-between p-4 text-white relative z-[10]">
+          {/* Movie Title */}
+          <h3 className="text-lg text-white font-bold mb-2">{show?.title}</h3>
+         
+          {/* Watch Trailer Button */}
+          <div className="w-full flex justify-between">
+            <span className="text-gray-400">{formatYear(show?.release_date)}</span>
+            <span className="text-yellow-400">★ {show?.vote_average?.toFixed(1)}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default SimilarCard;
