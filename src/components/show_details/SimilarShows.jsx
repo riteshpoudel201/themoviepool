a6d6a5e0b2b1a317e1e2869d84dd1c5040a60ca5
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-10 19:56:34 +0545

    add: created a component which loops through every shows and renders that individual show in SimilarCard component

---
+++ src/components/show_details/SimilarShows.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import SimilarCard from "../cards/SimilarCard";
+import { fetchSimilarShows } from "../../utils/axios";
+
+const SimilarShows = ({ showId, type }) => {
+  const [shows, setshows] = useState(null);
+  useEffect(() => {
+    const fetchRelatedShow = async () => {
+      const show = await fetchSimilarShows(showId, type);
+      setshows(show.results);
+    };
+    fetchRelatedShow();
+  }, [showId, type]);
+  console.log("Similar show lists: ", shows);
+  return (
+    <div className="w-full lg:w-[40%] flex flex-col gap-8 p-6 bg-white rounded-lg shadow-lg">
+      <h1 className="text-4xl text-purple-800 tracking-wider leading-3 text-center font-bold">
+        Similar
+      </h1>
+      <div className="flex flex-col gap-3 h-[80vh] overflow-x-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-purple-800">
+        {shows &&
+          shows.map((show) => (
+            <SimilarCard key={show.id} show={show} type={type} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SimilarShows;
