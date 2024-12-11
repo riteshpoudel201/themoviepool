7e7db570c56d19636eee4a65bfc5ada233b8fb4e
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-11 19:56:36 +0545

    add: added the function to fetch recommended shows, created the recommendation card, and created the component which loops through each shows and shows them in recommendation card. Also, the recommended shows are adjusted in show details page

---
+++ src/components/show_details/RecommendedShows.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import RecommendedCard from "../cards/RecommendationCard";
+import { fetchRecommendedShows } from "../../utils/axios";
+
+const RecommendedShows = ({ showId, type }) => {
+  const [shows, setshows] = useState(null);
+  useEffect(() => {
+    const fetchRelatedShow = async () => {
+      const show = await fetchRecommendedShows(showId, type);
+      setshows(show.results);
+    };
+    fetchRelatedShow();
+  }, [showId, type]);
+  console.log("Recommended show lists: ", shows);
+  return (
+    <div className="w-full flex flex-col gap-8 p-6 bg-white rounded-lg shadow-lg">
+      <h1 className="text-4xl text-purple-800 tracking-wider leading-3 font-bold">
+        Recommended
+      </h1>
+      <div className="flex flex-col gap-3 h-[80vh] overflow-x-auto snap-x snap-mandatory scrollbar-thin scrollbar-track-transparent scrollbar-thumb-purple-800">
+        {shows &&
+          shows.map((show) => (
+            <RecommendedCard key={show.id} show={show} type={type} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedShows;
