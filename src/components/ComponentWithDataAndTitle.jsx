4d5c64f6d7173332ad6342fc5f8a210f05de0879
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-16 20:08:07 +0545

    modify: key is renamed to reRenderKey

--- src/components/ComponentWithDataAndTitle.jsx
+++ src/components/ComponentWithDataAndTitle.jsx
@@ -6,27 +6,35 @@ import ComponentContainerWithTitle from "./ComponentContainerWithTitle";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const ComponentWithDataAndTitle = ({title, data,key,showType='movie',path=""}) => {
+const ComponentWithDataAndTitle = ({
+  title,
+  data,
+  reRenderKey,
+  showType = "movie",
+  path = "",
+}) => {
   return (
-    <ComponentContainerWithTitle title={title} path = {path}>
-      <CardSwiperContainer key={key}>
-        {data.map((slide, index) => (
-          <SwiperSlide key={index} className="w-full h-[90vh]">
-            <Card
-              imgSrc={`${base_url}${slide.poster_path}`}
-              imgAlt={slide.title}
-              showTitle={slide.title || slide.name}
-              showDesc={slide.overview}
-              releaseDate={slide.release_date || slide.first_air_date}
-              voteAverage={slide.vote_average}
-              showType={showType}
-              showId={slide.id}
-            />
-          </SwiperSlide>
-        ))}
-      </CardSwiperContainer>
+    <ComponentContainerWithTitle title={title} path={path}>
+      {data && (
+        <CardSwiperContainer reRenderKey={reRenderKey}>
+          {data.map((slide, index) => (
+            <SwiperSlide key={index} className="w-full h-[90vh]">
+              <Card
+                imgSrc={`${base_url}${slide.poster_path}`}
+                imgAlt={slide.title}
+                showTitle={slide.title || slide.name}
+                showDesc={slide.overview}
+                releaseDate={slide.release_date || slide.first_air_date}
+                voteAverage={slide.vote_average}
+                showType={showType}
+                showId={slide.id}
+              />
+            </SwiperSlide>
+          ))}
+        </CardSwiperContainer>
+      )}
     </ComponentContainerWithTitle>
-  )
-}
+  );
+};
 
 export default ComponentWithDataAndTitle;
